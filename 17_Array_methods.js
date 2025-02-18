47e93e280487d580cea2a9a27cb160f24110070e
harshPareek-git <harshpareek.git@gmail.com>
2025-02-18 23:27:39 +0530

    oker

--- 17_Array_methods.js
+++ 17_Array_methods.js
@@ -1,2 +1,29 @@
-let arr = [1,2,3,4,5]
-console.log(arr.toString());
+let arr = [1, 2, 3, 4, 5];
+let b = arr.toString();
+console.log(b);
+
+let c = arr.join("_");
+console.log(c);
+
+//removes last element
+let d = arr.pop();
+console.log(d);
+
+//adds last element
+let e = arr.push();
+console.log(e);
+
+//removes first element
+let f = arr.shift();
+console.log(f);
+
+//adds first element
+let g = arr.unshift();
+console.log(g, "yassu di balle balle");
+
+let arr2 = [11, 22, 33, 44, 55];
+let new_arr = arr.concat(arr2);
+console.log(new_arr);
+
+let newArray = [51, 92, 23, 24, 35];
+console.log(newArray.sort());
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,2 @@
-love
+
+// console.log(d);
